ba43c7381ec9c962552439d6cbc80fa29d61085a
or mizrahi <ormizrahi777@gmail.com>
2020-10-25 19:25:13 +0200

    fixed angular cli common error

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 using WebApplication1.Models;
 
 namespace WebApplication1
@@ -64,7 +65,7 @@ namespace WebApplication1
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
                 spa.Options.SourcePath = "ClientApp";
-
+                spa.Options.StartupTimeout = new TimeSpan(0, 5, 0);
                 if (env.IsDevelopment())
                 {
                     spa.UseAngularCliServer(npmScript: "start");
